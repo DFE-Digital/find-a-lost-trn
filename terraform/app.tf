f8396906516b9b0ef4093f0e6af21479c5685cbd
Merril Mathew <merril.mathew@outlook.com>
2022-02-08 16:10:53 +0000

    Terraform fmt missed

--- terraform/app.tf
+++ terraform/app.tf
@@ -5,13 +5,13 @@ resource "cloudfoundry_route" "fmt_public" {
 }
 
 resource "cloudfoundry_app" "api" {
-  name                       = var.fmt_app_name
-  space                      = data.cloudfoundry_space.space.id
-  instances                  = var.fmt_instances
-  memory                     = var.fmt_memory
-  disk_quota                 = var.fmt_disk_quota
-  docker_image               = var.fmt_docker_image
-  strategy                   = "blue-green"
+  name         = var.fmt_app_name
+  space        = data.cloudfoundry_space.space.id
+  instances    = var.fmt_instances
+  memory       = var.fmt_memory
+  disk_quota   = var.fmt_disk_quota
+  docker_image = var.fmt_docker_image
+  strategy     = "blue-green"
 
   routes {
     route = cloudfoundry_route.fmt_public.id
