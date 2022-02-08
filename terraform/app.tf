71269796c90e0dbe09442fca03232907bd95b31b
Merril Mathew <merril.mathew@outlook.com>
2022-02-08 17:38:18 +0000

    Add health check type

--- terraform/app.tf
+++ terraform/app.tf
@@ -5,13 +5,14 @@ resource "cloudfoundry_route" "fmt_public" {
 }
 
 resource "cloudfoundry_app" "app" {
-  name         = var.fmt_app_name
-  space        = data.cloudfoundry_space.space.id
-  instances    = var.fmt_instances
-  memory       = var.fmt_memory
-  disk_quota   = var.fmt_disk_quota
-  docker_image = var.fmt_docker_image
-  strategy     = "blue-green"
+  name              = var.fmt_app_name
+  space             = data.cloudfoundry_space.space.id
+  instances         = var.fmt_instances
+  memory            = var.fmt_memory
+  disk_quota        = var.fmt_disk_quota
+  docker_image      = var.fmt_docker_image
+  strategy          = "blue-green"
+  health_check_type = "http"
 
   routes {
     route = cloudfoundry_route.fmt_public.id
