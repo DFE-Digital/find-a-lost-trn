a26f1efbc2e094718f887d60d8343e0e3bd01810
Merril Mathew <merril.mathew@outlook.com>
2022-02-09 08:48:03 +0000

    Add port 3000 to PaaS route

--- terraform/app.tf
+++ terraform/app.tf
@@ -2,18 +2,17 @@ resource "cloudfoundry_route" "fmt_public" {
   domain   = data.cloudfoundry_domain.cloudapps.id
   hostname = var.fmt_app_name
   space    = data.cloudfoundry_space.space.id
+  port     = 3000
 }
 
 resource "cloudfoundry_app" "app" {
-  name                       = var.fmt_app_name
-  space                      = data.cloudfoundry_space.space.id
-  instances                  = var.fmt_instances
-  memory                     = var.fmt_memory
-  disk_quota                 = var.fmt_disk_quota
-  docker_image               = var.fmt_docker_image
-  strategy                   = "blue-green"
-  health_check_type          = "http"
-  health_check_http_endpoint = "/"
+  name         = var.fmt_app_name
+  space        = data.cloudfoundry_space.space.id
+  instances    = var.fmt_instances
+  memory       = var.fmt_memory
+  disk_quota   = var.fmt_disk_quota
+  docker_image = var.fmt_docker_image
+  strategy     = "blue-green"
 
   routes {
     route = cloudfoundry_route.fmt_public.id
