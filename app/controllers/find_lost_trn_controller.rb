25639e9ebdd9dd50d43cd4418a21c9daf985cf62
Theodor Vararu <theo@vararu.org>
2022-02-09 17:04:55 +0000

    Add a couple pages to the main journey

--- app/controllers/find_lost_trn_controller.rb
+++ app/controllers/find_lost_trn_controller.rb
@@ -7,6 +7,17 @@ class FindLostTrnController < ApplicationController
     class_name: 'Teacher',
     edit_path_helper: :teacher_edit_path,
     update_path_helper: :teacher_update_path,
-    pages: [Wizardry::Pages::QuestionPage.new(:check_trn, title: 'Check if you have a TRN', questions: [])],
+    pages: [
+      Wizardry::Pages::QuestionPage.new(
+        :check_trn,
+        title: 'Check if you have a TRN',
+        questions: [Wizardry::Questions::Radios.new(:do_you_have_a_trn, '1' => 'Yes', '0' => 'No')],
+      ),
+      Wizardry::Pages::QuestionPage.new(
+        :ask_questions,
+        title: 'We’ll ask you some questions to help find your TRN',
+        questions: [],
+      ),
+    ],
   )
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 class Teacher < ApplicationRecord
+  attr_accessor :do_you_have_a_trn
 end
