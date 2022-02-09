d955417f2b241f0ed5be0fddd57f307eb60c021a
Theodor Vararu <theo@vararu.org>
2022-02-09 14:00:56 +0000

    Rename submitted to complete in Teacher model

---
+++ db/migrate/20220209115739_rename_submitted_to_complete.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+class RenameSubmittedToComplete < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :teachers, :submitted, :complete
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_08_150451) do
+ActiveRecord::Schema.define(version: 2022_02_09_115739) do
   create_table 'teachers', force: :cascade do |t|
     t.string 'identifier'
     t.string 'last_completed_step'
-    t.boolean 'submitted'
+    t.boolean 'complete'
     t.string 'first_name'
     t.string 'last_name'
     t.string 'previous_first_name'
