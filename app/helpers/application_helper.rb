0999d3330d7638b8441c399d1aa2abc56ad9103f
Theodor Vararu <theo@vararu.org>
2022-02-09 13:59:46 +0000

    Patch turbo_frame_tag
    govuk-wizardry depends on this in order to render correctly. The
    behaviour needs to be updated upstream, but for now, we can just patch
    it in our helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # TODO: Upstream this behaviour into govuk-wizardry so we don't need this.
+  def turbo_frame_tag(id, &block)
+    tag.div(id: id, &block)
+  end
 end
