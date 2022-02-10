73327c0013fbf2b34b4874d1e172f059b86cceb2
Theodor Vararu <theo@vararu.org>
2022-02-10 12:11:16 +0000

    Redirect root to /start, clean up start

--- app/views/pages/_header.html.md
+++
@@ -1,3 +0,0 @@
-### Find My TRN
-
-Your _application_ is **ready** - so long as this page rendered without any errors you're good to go.
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'pages#start'
+  root to: redirect('/start')
+
+  get '/start', to: 'pages#start'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
