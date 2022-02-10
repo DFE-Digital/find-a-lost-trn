7a840f76ebae04291579f0b60a8d706f33be3d2b
Merril Mathew <merril.mathew@outlook.com>
2022-02-10 09:13:07 +0000

    Update terraform prefix and change app name.
    Updated prefix from fmt to flt,

--- terraform/app.tf
+++ terraform/app.tf
@@ -1,19 +1,19 @@
-resource "cloudfoundry_route" "fmt_public" {
+resource "cloudfoundry_route" "flt_public" {
   domain   = data.cloudfoundry_domain.cloudapps.id
-  hostname = var.fmt_app_name
+  hostname = var.flt_app_name
   space    = data.cloudfoundry_space.space.id
 }
 
 resource "cloudfoundry_app" "app" {
-  name         = var.fmt_app_name
+  name         = var.flt_app_name
   space        = data.cloudfoundry_space.space.id
-  instances    = var.fmt_instances
-  memory       = var.fmt_memory
-  disk_quota   = var.fmt_disk_quota
-  docker_image = var.fmt_docker_image
+  instances    = var.flt_instances
+  memory       = var.flt_memory
+  disk_quota   = var.flt_disk_quota
+  docker_image = var.flt_docker_image
   strategy     = "blue-green"
 
   routes {
-    route = cloudfoundry_route.fmt_public.id
+    route = cloudfoundry_route.flt_public.id
   }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,3 @@
-output "fmt_fqdn" {
+output "flt_fqdn" {
   value = "${cloudfoundry_route.fmt_public.hostname}.${data.cloudfoundry_domain.cloudapps.name}"
 }
