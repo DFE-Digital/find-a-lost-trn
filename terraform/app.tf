02566ddfc15a7e9a0ff127cea116bf8e08386703
Merril Mathew <merril.mathew@outlook.com>
2022-02-15 15:29:48 +0000

    Add terraform code
    The app and backing service needed for Find a Lost TRN will be deployed
    using terraform. This will follow teacher service standards and other
    deployments in TRA project.

---
+++ terraform/app.tf
@@ -0,0 +1,42 @@
+locals {
+  secret_key_base = {
+    SECRET_KEY_BASE = local.infrastructure_secrets.SECRET_KEY_BASE
+  }
+}
+resource "cloudfoundry_route" "flt_public" {
+  domain   = data.cloudfoundry_domain.cloudapps.id
+  hostname = var.flt_app_name
+  space    = data.cloudfoundry_space.space.id
+}
+
+resource "cloudfoundry_route" "flt_internal" {
+  domain   = data.cloudfoundry_domain.internal.id
+  space    = data.cloudfoundry_space.space.id
+  hostname = var.flt_app_name
+}
+resource "cloudfoundry_service_instance" "postgres" {
+  name         = var.postgres_database_name
+  space        = data.cloudfoundry_space.space.id
+  service_plan = data.cloudfoundry_service.postgres.service_plans[var.postgres_database_service_plan]
+}
+
+
+resource "cloudfoundry_app" "app" {
+  name         = var.flt_app_name
+  space        = data.cloudfoundry_space.space.id
+  instances    = var.flt_instances
+  memory       = var.flt_memory
+  disk_quota   = var.flt_disk_quota
+  docker_image = var.flt_docker_image
+  strategy     = "blue-green"
+  environment  = local.secret_key_base
+  dynamic "routes" {
+    for_each = local.flt_routes
+    content {
+      route = routes.value.id
+    }
+  }
+  service_binding {
+    service_instance = cloudfoundry_service_instance.postgres.id
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,64 @@
+variable "environment_name" {
+  type = string
+}
+
+variable "azure_sp_credentials_json" {
+  type = string
+}
+
+variable "key_vault_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "paas_api_url" {
+  default = "https://api.london.cloud.service.gov.uk"
+}
+
+variable "paas_org_name" {
+  type    = string
+  default = "dfe"
+}
+
+variable "paas_space" {
+  type = string
+}
+
+variable "flt_app_name" {
+  type = string
+}
+
+variable "flt_docker_image" {
+  type = string
+}
+
+variable "flt_instances" {
+  default = 1
+}
+
+variable "flt_memory" {
+  default = "1024"
+}
+
+variable "flt_disk_quota" {
+  default = "2048"
+}
+
+variable "postgres_database_name" {
+  type = string
+}
+
+variable "postgres_database_service_plan" {
+  type    = string
+  default = "small-13"
+}
+
+locals {
+  flt_routes = flatten([
+    cloudfoundry_route.flt_public,
+    cloudfoundry_route.flt_internal
+  ])
+}
