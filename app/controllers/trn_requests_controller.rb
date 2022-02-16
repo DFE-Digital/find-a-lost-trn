26f30e6ebc8029bf7dcf2610884acbebb61889f2
Felix Clack <felixclack@gmail.com>
2022-02-16 13:27:42 +0000

    Add the Check your answers page
    After providing all the details required to find a lost TRN, there is a
    screen that displays the answers given and presents an opportunity to
    modify them before submitting the request.
    
    The reference design for this screen is [here](https://find-a-lost-trn.herokuapp.com/check-answers) and the
    Trello ticket is [here](https://trello.com/c/cVaeihfv).
    
    Assumptions:
    * the column types for the fields have been set to what appears to be
      the correct type. This could change as we implement the relevant
      screens.
    * the `checked_at` field is a placeholder to represent the submit action
      by the user. This could be replaced by a finite state machine in
      future to better represent the state of the TRN request.
    * the back link goes to /start as that also happens to be the previous
      screen. Once we add more of the flow, this will change.
    * the TRN request values are hard-coded until we implement the screens
      to capture them.

--- app/controllers/trn_requests_controller.rb
+++ app/controllers/trn_requests_controller.rb
@@ -1,8 +1,35 @@
 # frozen_string_literal: true
 class TrnRequestsController < ApplicationController
   def create
-    @trn_request = TrnRequest.create
+    @trn_request =
+      TrnRequest.create(
+        date_of_birth: '1955-11-12',
+        email: 'email@example.com',
+        name: 'Jane Doe',
+        ni_number: 'QQ123456C',
+      )
     session[:trn_request_id] = @trn_request.id
+    redirect_to check_answers_url
+  end
+
+  def show
+    redirect_to root_url unless trn_request
+  end
+
+  def update
+    redirect_to root_url unless trn_request
+
+    trn_request.update(trn_request_params)
     redirect_to helpdesk_request_submitted_url
+  end
+
+  private
+
+  def trn_request
+    @trn_request ||= TrnRequest.find_by(id: session[:trn_request_id])
+  end
+
+  def trn_request_params
+    params.require(:trn_request).permit(:answers_checked)
   end
 end
---
+++ config/initializers/date_formats.rb
@@ -0,0 +1,2 @@
+# frozen_string_literal: true
+Date::DATE_FORMATS[:long_ordinal_uk] = '%d %B %Y'
