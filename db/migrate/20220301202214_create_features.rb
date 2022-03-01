018198a1bcadec01e7ed33654b973e98c8437cc9
Theodor Vararu <theo@vararu.org>
2022-03-01 20:23:50 +0000

    Add features table

---
+++ db/migrate/20220301202214_create_features.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+class CreateFeatures < ActiveRecord::Migration[7.0]
+  def change
+    create_table :features do |t|
+      t.string :name, null: false
+      t.boolean :active, default: false, null: false
+      t.timestamps
+    end
+
+    add_index :features, :name, unique: true
+  end
+end
