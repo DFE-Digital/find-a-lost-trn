7710fb007b9a3cbe77261541996511de997f881b
Theodor Vararu <theo@vararu.org>
2022-03-01 20:23:50 +0000

    Add features table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_23_110719) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_01_202214) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'features', force: :cascade do |t|
+    t.string 'name', null: false
+    t.boolean 'active', default: false, null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['name'], name: 'index_features_on_name', unique: true
+  end
 
   create_table 'trn_requests', force: :cascade do |t|
     t.datetime 'created_at', null: false
