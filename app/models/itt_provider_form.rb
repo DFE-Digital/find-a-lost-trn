349a817607039a204bc3bb913bc7a454bc3fc780
Theodor Vararu <theo@vararu.org>
2022-03-03 17:08:16 +0000

    Create IttProviderForm object

---
+++ app/models/itt_provider_form.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+class IttProviderForm
+  include ActiveModel::Model
+
+  attr_accessor :trn_request, :itt_provider_enrolled, :itt_provider_name
+
+  validates :itt_provider_enrolled, inclusion: { in: %w[true false] }
+  validates :itt_provider_name, presence: true, length: { maximum: 255 }, if: -> { itt_provider_enrolled == 'true' }
+
+  def save
+    return false if invalid?
+
+    trn_request.update(itt_provider_enrolled: itt_provider_enrolled, itt_provider_name: itt_provider_name)
+  end
+
+  def assign_form_values
+    self.itt_provider_enrolled = trn_request.itt_provider_enrolled
+    self.itt_provider_name = trn_request.itt_provider_name
+    self
+  end
+end
