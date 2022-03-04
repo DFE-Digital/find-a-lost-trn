c533ecdd5b172a4ecb15538f6507bbecf8272fa3
Felix Clack <felixclack@gmail.com>
2022-03-04 11:19:19 +0000

    Ensure the session ID is set correctly
    We store the TrnRequest#id in the session to be able to pass state
    between screens. The value needs to be set when the first question is
    answered.
    
    This change was missed when we added the NI questions and as a result
    the TrnRequest doesn't persist correctly between questions.
    
    As we add new questions, this step will continue to migrate to the front
    of the flow and we will need to add better tests to ensure we don't miss
    this in the future.
    
    For now, I avoided adding extra tests as there are other issues to be
    fixed before we can do that.

--- app/controllers/itt_providers_controller.rb
+++ app/controllers/itt_providers_controller.rb
@@ -5,8 +5,7 @@ class IttProvidersController < ApplicationController
   end
 
   def update
-    if trn_request.update!(itt_provider_params)
-      session[:trn_request_id] = trn_request.id
+    if trn_request.update(itt_provider_params)
       redirect_to trn_request.email.blank? ? email_url : check_answers_url
     else
       render :edit
@@ -20,6 +19,6 @@ class IttProvidersController < ApplicationController
   end
 
   def trn_request
-    @trn_request ||= TrnRequest.find_by(id: session[:trn_request_id]) || TrnRequest.new
+    @trn_request ||= TrnRequest.find(session[:trn_request_id])
   end
 end
