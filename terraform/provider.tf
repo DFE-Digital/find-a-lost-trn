ace0f92c73a734255b76a55743c343b32271456d
Merril Mathew <merril.mathew@outlook.com>
2022-03-10 16:05:28 +0000

    Add Status cake credential for the provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,7 @@
 locals {
   azure_credentials      = jsondecode(var.azure_sp_credentials_json)
   infrastructure_secrets = yamldecode(data.azurerm_key_vault_secret.secrets["INFRASTRUCTURE"].value)
+  monitoring_secrets     = yamldecode(data.azurerm_key_vault_secret.secrets["MONITORING"].value)
 }
 
 provider "azurerm" {
@@ -17,4 +18,9 @@ provider "cloudfoundry" {
   api_url  = var.paas_api_url
   user     = data.azurerm_key_vault_secret.secrets["PAAS-USER"].value
   password = data.azurerm_key_vault_secret.secrets["PAAS-PASSWORD"].value
+}
+
+provider "statuscake" {
+  username = local.monitoring_secrets.STATUSCAKE_USERNAME
+  apikey   = local.monitoring_secrets.STATUSCAKE_PASSWORD
 }
