85c2fae8350c8d0a93695bfac8756d5ecbc66b83
Felix Clack <felixclack@gmail.com>
2022-03-11 08:25:56 +0000

    Add length validations to all name fields
    We want to ensure that the name fields don't exceed the database column
    limit of 255 characters.

--- spec/forms/name_form_spec.rb
+++ spec/forms/name_form_spec.rb
@@ -4,6 +4,10 @@ require 'rails_helper'
 RSpec.describe NameForm, type: :model do
   it { is_expected.to validate_presence_of(:first_name).with_message('Enter your first name') }
   it { is_expected.to validate_presence_of(:last_name).with_message('Enter your last name') }
+  it { is_expected.to validate_length_of(:first_name).is_at_most(255) }
+  it { is_expected.to validate_length_of(:last_name).is_at_most(255) }
+  it { is_expected.to validate_length_of(:previous_first_name).is_at_most(255) }
+  it { is_expected.to validate_length_of(:previous_last_name).is_at_most(255) }
 
   describe '#save' do
     subject(:save) { name_form.save }
