5a734210b53751bfc1b1d55fb0312c2c59eb34e8
Felix Clack <felixclack@gmail.com>
2022-03-25 11:22:15 +0000

    Fix VCR tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -90,5 +90,5 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.configure_rspec_metadata!
   config.ignore_localhost = true
-  config.default_cassette_options = { record: :new_episodes, erb: true }
+  config.default_cassette_options = { record: :once, erb: true }
 end
--- spec/services/dqt_api_spec.rb
+++ spec/services/dqt_api_spec.rb
@@ -1,29 +1,47 @@
 # frozen_string_literal: true
 require 'rails_helper'
+require 'webmock/rspec'
 
 RSpec.describe DqtApi do
-  describe '.find_trn!', vcr: true do
+  describe '.find_trn!' do
     subject(:find_trn!) { described_class.find_trn!(trn_request) }
 
     let(:trn_request) do
       TrnRequest.new(date_of_birth: '1990-01-01', email: 'kevin@kevin.com', first_name: 'kevin', ni_number: '1000000')
     end
 
-    it { is_expected.to match(hash_including('trn' => '1275362')) }
-    it { is_expected.to match(hash_including('emailAddresses' => ['anonymous@anonymousdomain.org.net.co.uk'])) }
-    it { is_expected.to match(hash_including('firstName' => 'Kevin')) }
-    it { is_expected.to match(hash_including('lastName' => 'Evans')) }
-    it { is_expected.to match(hash_including('dateOfBirth' => '1990-01-01')) }
-    it { is_expected.to match(hash_including('nationalInsuranceNumber' => 'SD78771P9')) }
-    it { is_expected.to match(hash_including('uid' => 'f7891223-7661-e411-8047-005056822391')) }
+    context 'with a matching TRN request', vcr: true do
+      it { is_expected.to match(hash_including('trn' => '1275362')) }
+      it { is_expected.to match(hash_including('emailAddresses' => ['anonymous@anonymousdomain.org.net.co.uk'])) }
+      it { is_expected.to match(hash_including('firstName' => 'Kevin')) }
+      it { is_expected.to match(hash_including('lastName' => 'Evans')) }
+      it { is_expected.to match(hash_including('dateOfBirth' => '1990-01-01')) }
+      it { is_expected.to match(hash_including('nationalInsuranceNumber' => 'SD78771P9')) }
+      it { is_expected.to match(hash_including('uid' => 'f7891223-7661-e411-8047-005056822391')) }
+    end
 
     context 'when the API returns a timeout error' do
-      before { FeatureFlag.activate(:dqt_api_always_timeout) }
-
-      after { FeatureFlag.deactivate(:dqt_api_always_timeout) }
-
       it 'raises a timeout error' do
-        expect { find_trn! }.to raise_error(Faraday::TimeoutError)
+        VCR.turned_off do
+          allow_any_instance_of(Faraday::Connection).to receive(:get).and_raise(Faraday::TimeoutError) # rubocop:disable RSpec/AnyInstance
+          expect { find_trn! }.to raise_error(Faraday::TimeoutError)
+        end
+      end
+    end
+
+    context 'when the API returns multiple results', vcr: true do
+      let(:trn_request) do
+        TrnRequest.new(
+          date_of_birth: '1990-01-01',
+          email: 'test@example.com',
+          first_name: 'John',
+          last_name: 'Smith',
+          ni_number: 'QQ123456C',
+        )
+      end
+
+      it 'raises an error' do
+        expect { find_trn! }.to raise_error(DqtApi::TooManyResults)
       end
     end
   end
