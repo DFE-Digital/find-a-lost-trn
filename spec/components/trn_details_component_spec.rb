f7d8151720db723bbd4db65666b5c95b75d9f52e
Theodor Vararu <theo@vararu.org>
2022-04-15 12:34:35 +0100

    Create TrnDetailsComponent

---
+++ spec/components/trn_details_component_spec.rb
@@ -0,0 +1,109 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe TrnDetailsComponent, type: :component do
+  let(:trn_request) { TrnRequest.new(
+    date_of_birth: 20.years.ago.to_date,
+    email: 'test@example.com',
+    first_name: 'Test',
+    has_ni_number: true,
+    itt_provider_enrolled: true,
+    itt_provider_name: 'Big SCITT',
+    last_name: 'User',
+    ni_number: 'QC123456A',
+    previous_last_name: 'Smith',
+  ) }
+  let(:component) { render_inline(described_class.new(trn_request: trn_request)) }
+
+  it 'renders name' do
+    expect(component.text).to include('Test User')
+  end
+
+  it 'renders previous name' do
+    expect(component.text).to include('Test Smith')
+  end
+
+  it 'renders date of birth' do
+    expect(component.text).to include(20.years.ago.to_date.to_fs(:long_ordinal_uk))
+  end
+
+  it 'renders NI number' do
+    expect(component.text).to include('QC 12 34 56 A')
+  end
+
+  it 'renders ITT provider' do
+    expect(component.text).to include('Big SCITT')
+  end
+
+  it 'renders email' do
+    expect(component.text).to include('test@example.com')
+  end
+
+  it 'does not render change buttons' do
+    expect(component.text).not_to include('Change')
+  end
+
+  context 'with actions' do
+    let(:component) { render_inline(described_class.new(trn_request: trn_request, actions: true)) }
+
+    it 'renders change name' do
+      expect(component.text).to include('Change name')
+    end
+
+    it 'renders change previous name' do
+      expect(component.text).to include('Change previous name')
+    end
+
+    it 'renders change date of birth' do
+      expect(component.text).to include('Change date of birth')
+    end
+
+    it 'renders change NI number' do
+      expect(component.text).to include('Change national insurance number')
+    end
+
+    it 'renders change ITT provider' do
+      expect(component.text).to include('Change teacher training provider')
+    end
+
+    it 'renders change email' do
+      expect(component.text).to include('Change email')
+    end
+  end
+
+  context 'with anonymised data' do
+    let(:component) { render_inline(described_class.new(trn_request: trn_request, anonymise: true)) }
+
+    it 'renders anonymised name' do
+      expect(component.text).to include('T**** U****')
+    end
+
+    it 'renders anonymised previous name' do
+      expect(component.text).to include('T**** S****')
+    end
+
+    it 'renders anonymised date of birth' do
+      expect(component.text).to include('** ** ****')
+    end
+
+    it 'renders anonymised NI number' do
+      expect(component.text).to include('Q* ** ** ** A')
+    end
+
+    it 'renders ITT provider' do
+      expect(component.text).to include('Big SCITT')
+    end
+
+    it 'renders anonymised email' do
+      expect(component.text).to include('t****@****.com')
+    end
+  end
+
+  context 'with both actions and anonymised data' do
+    it 'raises an error' do
+      expect {
+        render_inline(described_class.new(trn_request: trn_request, anonymise: true, actions: true))
+      }.to raise_error(ArgumentError)
+    end
+  end
+end
