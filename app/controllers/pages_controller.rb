01a77f17331a7954fedec0853b8867044674c28f
Theodor Vararu <theo@vararu.org>
2022-04-20 16:54:37 +0100

    Add performance dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,4 +16,24 @@ class PagesController < ApplicationController
   def start
     session[:form_complete] = false
   end
+
+  def performance # rubocop:disable Metrics/AbcSize
+    trn_requests = TrnRequest.order(created_at: :desc).where(created_at: 1.week.ago.beginning_of_day..Time.zone.now)
+
+    trn_requests_from_days_ago = (0..6).map { |n| trn_requests.where(created_at: n.days.ago.all_day) }
+
+    @requests_over_last_7_days = trn_requests.count
+
+    @live_service_data = [%w[Date Requests]]
+    @live_service_data +=
+      trn_requests_from_days_ago.map.with_index { |reqs, i| [i.days.ago.strftime('%d %B'), reqs.count] }
+
+    @trns_found = trn_requests.where.not(trn: nil).count
+
+    @submission_data = [['Date', 'TRNs found', 'Zendesk tickets opened']]
+    @submission_data +=
+      trn_requests_from_days_ago.map.with_index do |reqs, i|
+        [i.days.ago.strftime('%d %B'), reqs.where.not(trn: nil).count, reqs.where.not(zendesk_ticket_id: nil).count]
+      end
+  end
 end
---
+++ spec/system/performance_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe 'Performance', type: :system do
+  before { Timecop.freeze(Date.new(2022, 0o4, 21)) }
+
+  after { Timecop.return }
+
+  it 'using the performance dashboard' do
+    given_the_service_is_open
+    given_there_are_a_few_trns
+    when_i_visit_the_performance_page
+    then_i_see_the_live_stats
+  end
+
+  private
+
+  def given_the_service_is_open
+    FeatureFlag.activate(:service_open)
+  end
+
+  def given_there_are_a_few_trns
+    (0..6).each.with_index { |n, i| (i + 1).times { create(:trn_request, created_at: n.days.ago) } }
+  end
+
+  def when_i_visit_the_performance_page
+    visit performance_path
+  end
+
+  def then_i_see_the_live_stats
+    expect(page).to have_content("21 April\t1")
+    expect(page).to have_content("15 April\t7")
+  end
+end
