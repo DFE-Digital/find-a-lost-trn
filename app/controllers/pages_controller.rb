92885f1105094841b3ec3ca5af6c817daf3332bc
Theodor Vararu <theo@vararu.org>
2022-04-21 13:01:54 +0100

    Drastically reduce number of queries for perf page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,23 +17,35 @@ class PagesController < ApplicationController
     session[:form_complete] = false
   end
 
-  def performance # rubocop:disable Metrics/AbcSize
-    trn_requests = TrnRequest.order(created_at: :desc).where(created_at: 1.week.ago.beginning_of_day..Time.zone.now)
+  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Metrics/PerceivedComplexity
+  def performance
+    trn_requests =
+      TrnRequest.where(created_at: 1.week.ago.beginning_of_day..Time.zone.now).group("date_trunc('day', created_at)")
 
-    trn_requests_from_days_ago = (0..6).map { |n| trn_requests.where(created_at: n.days.ago.all_day) }
+    trn_requests_total = trn_requests.count
 
-    @requests_over_last_7_days = trn_requests.count
+    trn_requests_with_trn = trn_requests.where.not(trn: nil).count
+
+    trn_requests_with_zendesk_ticket = trn_requests.where.not(zendesk_ticket_id: nil).count
+
+    last_7_days = (0..6).map { |n| n.days.ago.beginning_of_day.utc }
+
+    @requests_over_last_7_days = trn_requests_total.values.reduce(&:+)
 
     @live_service_data = [%w[Date Requests]]
-    @live_service_data +=
-      trn_requests_from_days_ago.map.with_index { |reqs, i| [i.days.ago.strftime('%d %B'), reqs.count] }
+    @live_service_data += last_7_days.map { |day| [day.strftime('%d %B'), trn_requests_total[day] || 0] }
 
-    @trns_found = trn_requests.where.not(trn: nil).count
+    @trns_found = trn_requests_with_trn.values.reduce(&:+)
 
     @submission_data = [['Date', 'TRNs found', 'Zendesk tickets opened']]
     @submission_data +=
-      trn_requests_from_days_ago.map.with_index do |reqs, i|
-        [i.days.ago.strftime('%d %B'), reqs.where.not(trn: nil).count, reqs.where.not(zendesk_ticket_id: nil).count]
+      last_7_days.map do |day|
+        [day.strftime('%d %B'), trn_requests_with_trn[day] || 0, trn_requests_with_zendesk_ticket[day] || 0]
       end
   end
+  # rubocop:enable Metrics/AbcSize
+  # rubocop:enable Metrics/CyclomaticComplexity
+  # rubocop:enable Metrics/PerceivedComplexity
 end
