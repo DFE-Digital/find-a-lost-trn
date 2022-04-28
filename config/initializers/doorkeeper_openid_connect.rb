ddc72e124bb80ea9c1a7e9695be014b0d802fa99
Theodor Vararu <theo@vararu.org>
2022-04-28 14:35:31 +0100

    Configure signing_key
    https://github.com/doorkeeper-gem/doorkeeper-openid_connect#configuration
    
    Use `openssl genpkey -algorithm RSA -out private_key.pem -pkeyopt
    rsa_keygen_bits:2048`
    
    Yes, this key shouldn't be commited to version control.
    
    This is a spike, I don't expect this code to go into production.
    
    Please DO NOT use this key for anything important.

--- config/initializers/doorkeeper_openid_connect.rb
+++ config/initializers/doorkeeper_openid_connect.rb
@@ -4,9 +4,34 @@ Doorkeeper::OpenidConnect.configure do
   issuer 'https://dev-find-a-lost-trn.education.gov.uk/'
 
   signing_key <<~KEY
-    -----BEGIN RSA PRIVATE KEY-----
-    ....
-    -----END RSA PRIVATE KEY-----
+    -----BEGIN PRIVATE KEY-----
+    MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCvm+EQvZfmgusD
+    b9toQbxcy+i6cjILSFENMnTPwdBy3zwKwbr921WnD80PiC5nls+9Ue240ma3WeK+
+    V0PjSd4xjKAg567wwxBkVxiW2ERcQ8ENL3PbhHCwFZg6IAs9IXKljsNyV3D9hpqH
+    kiiAvfdsVS/Jk7ar3tOrtSuxgPTLLuvjZsWJLGw5zNGqD7mtM4MQcIBlcpG7zwj9
+    zbKV/X9pjzIATabDrb9BbE84e3wZzmq22njS/7lwdhowO/++wf660FSBi2z4DzmH
+    ryC1tG8SgVIawh2cKFPqrUyL+dfXMSrJve0aEKKUabQ8U1qB4WLBrxhAk19jjJHx
+    uK6BxdtjAgMBAAECggEBAKFWclHCDnPOpgJTW14OvvL2uEvLrOSmvSiFycIyi8Nu
+    CyjK0nR2ddv17W0urYuoiBWk1uYuEAa2A1bir9F7RTN6wodYtGYooL+/MS0tZqjp
+    6sylYyk0JZ/xRxln5Ut1lnI6lqBPa9TRPNn/U0zsuwwBu9TT7Ek9gHVKDumW09Bz
+    NW5/GlVFUqYZuu8e34cNmOn0hr39YWmtVHYAVJ7H2utADpT8KNjfzU5OBdSt5yyi
+    yGUeE4VE519ZI7xcyHJqQCLi6WSWIsyZoK3iUpg0khzGfGrYMnYpvrj9gPaqx3f8
+    I8iED3tECgOBpgrlfME1FVIPgkp/fInvGHeDTDzJPKECgYEA5s7IjGLAAZqXHFr/
+    h8vr0WmU5RormygyaIbugAgapiWmnMLHN7MSztWXTRrfVE/cG9cKjSWuPLIQF0r+
+    nN8D8jXeK6gc08cwtaALyHWouABQrc2Kyp0pHzZZ9ZHzyjgAEK7nxGmzh3zZ1l/W
+    DBQomLP6uigrk5MOeKtOEQjWrPECgYEAwsa7nbbdPYbD3CPwXjVs2qT8NtyytVv7
+    xWbeikIVpBWZVOuTDbwF6nBIPwS7/RdBcN39jklhRFS3NuZ6pU95d2X56vJOerP1
+    49aeUkhkJBRM8dSKlHmnW4CeljheHTjuZL/YpW24WLBFsoiz820bJFaMx0HZDZy3
+    fJaZvgB9XZMCgYEAnB/mIqgtrygN7X1UelwjQP1FXY/NTV/scS5MqEhTFm5DfKh4
+    r6GvKD/s35g810BJ6H8WwRFrAd2E6uysvBpTBgUQKsHF9i84MLN5JPkJzqVP7bEq
+    oBGdHmLaiTVYDkLBMIfPeNnzOcPnao5bMMJ2D403HbNMfr+ru05NOVKcPFECgYBO
+    ybDu1R+dWbw0el0q3pWPxBk54AByGCk56qiuaJj0bMA/d6NedOY3tP+kbjfU4ulX
+    zJRaUBiGWXZbQNPvtrw9rgRwI1/XhqA6b4BPbHUFmyic0grUkHlALED6Jwb67yKP
+    ooLyN/XP+k3XMDEKkOHfyxrbJymztecLAKhkETKk/QKBgQCg9pzM/Tkovm3Uokqq
+    HrPn0L4IjRInEQk/VEizQtgj7nCVQSb/57R6Colt4S/hfrAYKxPjdD9A2zlVeV3N
+    WMFF09K5aciyHJYgoXeidwOAofpbuDgfRkNrtJCYVk6RwcY1FfLQFGLk1BenNW4g
+    WQupXkNNu9trmWGtYFNVvcLkXQ==
+    -----END PRIVATE KEY-----
   KEY
 
   subject_types_supported [:public]
