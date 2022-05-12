7184ee368d9d756b7fcf871fd055554be92f92a2
Theodor Vararu <theo@vararu.org>
2022-05-12 12:11:32 +0200

    Allow seeing all performance stats ?since_launch
    Adds a `?since_launch` query string param to the performance dashboard
    that allows querying for the full data since May the 4th.
    
    This page will likely get slow over time, but we can worry about that
    later. Making it a query string param makes it slightly more hidden than
    the regular performance dashboard.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,8 +21,18 @@ class PagesController < ApplicationController
   # rubocop:disable Metrics/CyclomaticComplexity
   # rubocop:disable Metrics/PerceivedComplexity
   def performance
-    trn_requests =
-      TrnRequest.where(created_at: 1.week.ago.beginning_of_day..Time.zone.now).group("date_trunc('day', created_at)")
+    since = 1.week.ago.beginning_of_day
+    until_days = 6
+    @since_text = 'over the last 7 days'
+
+    if params.key? :since_launch
+      launch_date = Date.new(2022, 5, 4).beginning_of_day
+      since = launch_date
+      until_days = ((Time.zone.now.beginning_of_day - since) / (3600 * 24)).to_i
+      @since_text = 'since launch'
+    end
+
+    trn_requests = TrnRequest.where(created_at: since..Time.zone.now).group("date_trunc('day', created_at)")
 
     trn_requests_total = trn_requests.count
 
@@ -30,18 +40,18 @@ class PagesController < ApplicationController
 
     trn_requests_with_zendesk_ticket = trn_requests.where.not(zendesk_ticket_id: nil).count
 
-    last_7_days = (0..6).map { |n| n.days.ago.beginning_of_day.utc }
+    last_n_days = (0..until_days).map { |n| n.days.ago.beginning_of_day.utc }
 
-    @requests_over_last_7_days = trn_requests_total.values.reduce(&:+)
+    @requests_over_last_n_days = trn_requests_total.values.reduce(&:+)
 
     @live_service_data = [%w[Date Requests]]
-    @live_service_data += last_7_days.map { |day| [day.strftime('%d %B'), trn_requests_total[day] || 0] }
+    @live_service_data += last_n_days.map { |day| [day.strftime('%d %B'), trn_requests_total[day] || 0] }
 
     @trns_found = trn_requests_with_trn.values.reduce(&:+)
 
     @submission_data = [['Date', 'TRNs found', 'Zendesk tickets opened']]
     @submission_data +=
-      last_7_days.map do |day|
+      last_n_days.map do |day|
         [day.strftime('%d %B'), trn_requests_with_trn[day] || 0, trn_requests_with_zendesk_ticket[day] || 0]
       end
   end
