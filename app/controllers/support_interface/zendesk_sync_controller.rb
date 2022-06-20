43baa778b08415ce61c581afd5753cd51ac01f3e
Felix Clack <felixclack@gmail.com>
2022-06-20 09:47:28 +0100

    Enable iterative processing of Zendesk comments
    When searching for a TRN in the comments of a Zendesk ticket there is an
    assumption that the last comment contains the matching text.
    
    In practice, this isn't always true. A ticket can have multiple comments
    and the last one isn't guaranteed to contain the TRN.
    
    This change will iteratively process the comments, starting from the
    second one (the first is always a copy of the original request). The
    search will stop as soon as the regex matches.
    
    I opted for this as the simplest improvement we could make to the
    current process. It still makes an assumption that the regex will
    eventually match. This assumption may not always be true in practice.
    
    We are looking into ways of making this more reliable in the future.

--- app/controllers/support_interface/zendesk_sync_controller.rb
+++ app/controllers/support_interface/zendesk_sync_controller.rb
@@ -2,30 +2,9 @@
 module SupportInterface
   class ZendeskSyncController < SupportInterfaceController
     def create
-      update_from_zendesk if zendesk_ticket.comments.many?
+      trn_request = TrnRequest.find(params[:trn_request_id])
+      flash[:warning] = t('.warning') unless FetchTrnFromZendesk.new(trn_request: trn_request).call
       redirect_to support_interface_trn_requests_url
-    end
-
-    private
-
-    def trn
-      @trn ||= zendesk_ticket.comments.last.body.scan(/\*\*(\d{7})\*\*/).flatten.first
-    end
-
-    def trn_request
-      @trn_request ||= TrnRequest.find(params[:trn_request_id])
-    end
-
-    def update_from_zendesk
-      return if trn.blank?
-
-      raw_result = DqtApi.find_teacher!(date_of_birth: trn_request.date_of_birth, trn: trn)
-      trn_request.trn_responses.create(raw_result: raw_result)
-      trn_request.update(trn: raw_result['trn'])
-    end
-
-    def zendesk_ticket
-      @zendesk_ticket ||= ZendeskService.find_ticket(trn_request.zendesk_ticket_id)
     end
   end
 end
---
+++ spec/services/fetch_trn_from_zendesk_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe FetchTrnFromZendesk, type: :model do
+  it { is_expected.to validate_presence_of(:trn_request) }
+
+  describe '#call' do
+    subject(:fetch) { described_class.new(trn_request: trn_request).call }
+
+    let(:trn_request) { create(:trn_request, :has_zendesk_ticket) }
+    let(:raw_result) { { 'trn' => '2921020' } }
+
+    before { allow(DqtApi).to receive(:find_teacher!).and_return(raw_result) }
+
+    it 'updates the provided TrnRequest with a TRN' do
+      expect { fetch }.to change(trn_request, :trn).from(nil).to('2921020')
+    end
+
+    context 'when there is no TRN in the comments' do
+      let(:ticket) do
+        ZendeskAPI::Ticket
+          .new(GDS_ZENDESK_CLIENT, id: 1)
+          .tap do |ticket|
+            ticket.comments = [ZendeskAPI::Ticket::Comment.new(GDS_ZENDESK_CLIENT, id: 1, body: 'Example')]
+          end
+      end
+
+      before { allow(GDS_ZENDESK_CLIENT.ticket).to receive(:find).and_return(ticket) }
+
+      it 'does not update the TrnRequest' do
+        expect { fetch }.not_to change(trn_request, :trn)
+      end
+    end
+
+    context 'when the Zendesk ticket last comment is a customer response' do
+      let(:ticket) do
+        ZendeskAPI::Ticket
+          .new(GDS_ZENDESK_CLIENT, id: 1)
+          .tap do |ticket|
+            ticket.comments = [
+              ZendeskAPI::Ticket::Comment.new(GDS_ZENDESK_CLIENT, id: 1, body: 'Example'),
+              ZendeskAPI::Ticket::Comment.new(GDS_ZENDESK_CLIENT, id: 2, body: 'Your TRN is **2921020**'),
+              ZendeskAPI::Ticket::Comment.new(GDS_ZENDESK_CLIENT, id: 3, body: 'Thanks'),
+            ]
+          end
+      end
+
+      before { allow(GDS_ZENDESK_CLIENT.ticket).to receive(:find).and_return(ticket) }
+
+      it 'updates the provided TrnRequest with a TRN' do
+        expect { fetch }.to change(trn_request, :trn).from(nil).to('2921020')
+      end
+    end
+  end
+end
