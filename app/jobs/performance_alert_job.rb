f43d9dcfb934445e5f032b0072e7ce3cee1bb6af
Felix Clack <felixclack@gmail.com>
2022-06-23 13:00:53 +0100

    Customise the performance alert URL per host
    The URL that is included in the performance alert is hard-coded to
    production. This makes it hard to distinguish alerts from other
    environments.
    
    We occasionally need to test this alert in other environments, so it is
    beneficial to allow us to customise the URL.
    
    I opted to use an environment variable to store the current hosting
    domain, that can then be passed to the Rails URL helper to generate the
    appropriate value.
    
    This seems simpler and easier to extend across environments than the
    alternative of keeping a list of all expected domains in code somewhere.

--- app/jobs/performance_alert_job.rb
+++ app/jobs/performance_alert_job.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 class PerformanceAlertJob < ApplicationJob
   include ActionView::Helpers::TextHelper
+  include Rails.application.routes.url_helpers
 
   def perform
     count = TrnRequest.where(created_at: 1.week.ago.beginning_of_day..Time.zone.now).count
-    message =
-      "There have been #{pluralize(count, 'TRN request')} started in the last 7 days on https://find-a-lost-trn.education.gov.uk/performance"
+    url = performance_url(host: ENV.fetch('HOSTING_DOMAIN'))
+    message = "There have been #{pluralize(count, 'TRN request')} started in the last 7 days on #{url}"
     SlackClient.create_message(message)
   end
 end
