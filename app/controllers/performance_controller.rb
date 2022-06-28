c2374a9f610e849d5d835d02a5d6e07745b09259
Jake Benilov <benilov@gmail.com>
2022-06-28 17:12:13 +0100

    Extract PerformanceStats from PerformanceController
    This encapsulates the code better, and adds test coverage.

--- app/controllers/performance_controller.rb
+++ app/controllers/performance_controller.rb
@@ -2,44 +2,20 @@
 
 class PerformanceController < ApplicationController
   # rubocop:disable Metrics/AbcSize
-  # rubocop:disable Metrics/CyclomaticComplexity
-  # rubocop:disable Metrics/PerceivedComplexity
   def index
-    since = 1.week.ago.beginning_of_day
-    until_days = 6
+    time_period = 1.week.ago.beginning_of_day..Time.zone.now
     @since_text = 'over the last 7 days'
 
     if params.key? :since_launch
       launch_date = Date.new(2022, 5, 4).beginning_of_day
-      since = launch_date
-      until_days = ((Time.zone.now.beginning_of_day - since) / (3600 * 24)).to_i
+      time_period = launch_date..Time.zone.now
       @since_text = 'since launch'
     end
 
-    trn_requests = TrnRequest.where(created_at: since..Time.zone.now).group("date_trunc('day', created_at)")
+    stats = PerformanceStats.new(time_period)
 
-    trn_requests_total = trn_requests.count
-
-    trn_requests_with_trn = trn_requests.where.not(trn: nil).count
-
-    trn_requests_with_zendesk_ticket = trn_requests.where.not(zendesk_ticket_id: nil).count
-
-    last_n_days = (0..until_days).map { |n| n.days.ago.beginning_of_day.utc }
-
-    @requests_over_last_n_days = trn_requests_total.values.reduce(&:+)
-
-    @live_service_data = [%w[Date Requests]]
-    @live_service_data += last_n_days.map { |day| [day.strftime('%d %B'), trn_requests_total[day] || 0] }
-
-    @trns_found = trn_requests_with_trn.values.reduce(&:+)
-
-    @submission_data = [['Date', 'TRNs found', 'Zendesk tickets opened']]
-    @submission_data +=
-      last_n_days.map do |day|
-        [day.strftime('%d %B'), trn_requests_with_trn[day] || 0, trn_requests_with_zendesk_ticket[day] || 0]
-      end
+    @requests_over_last_n_days, @live_service_data = stats.live_service_usage
+    @trns_found, @submission_data = stats.submission_results
   end
   # rubocop:enable Metrics/AbcSize
-  # rubocop:enable Metrics/CyclomaticComplexity
-  # rubocop:enable Metrics/PerceivedComplexity
 end
---
+++ app/lib/performance_stats.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+# rubocop:disable Metrics/AbcSize
+class PerformanceStats
+  def initialize(time_period)
+    number_of_days_in_period = ((Time.zone.now.beginning_of_day - time_period.first) / 1.day).to_i
+
+    @trn_requests = TrnRequest.where(created_at: time_period).group("date_trunc('day', created_at)")
+
+    @last_n_days = (0..number_of_days_in_period).map { |n| n.days.ago.beginning_of_day.utc }
+
+    calculate_live_service_usage
+    calculate_submission_results
+  end
+
+  def live_service_usage
+    [@requests_over_last_n_days, @live_service_data]
+  end
+
+  def submission_results
+    [@trns_found, @submission_data]
+  end
+
+  private
+
+  def calculate_live_service_usage
+    trn_requests_total = @trn_requests.count
+
+    @requests_over_last_n_days = trn_requests_total.values.reduce(&:+)
+
+    @live_service_data = [%w[Date Requests]]
+    @live_service_data += @last_n_days.map { |day| [day.strftime('%d %B'), trn_requests_total[day] || 0] }
+  end
+
+  def calculate_submission_results
+    trn_requests_with_trn = @trn_requests.where.not(trn: nil).count
+    trn_requests_with_zendesk_ticket = @trn_requests.where.not(zendesk_ticket_id: nil).count
+
+    @trns_found = trn_requests_with_trn.values.reduce(&:+)
+
+    @submission_data = [['Date', 'TRNs found', 'Zendesk tickets opened']]
+    @submission_data +=
+      @last_n_days.map do |day|
+        [day.strftime('%d %B'), trn_requests_with_trn[day] || 0, trn_requests_with_zendesk_ticket[day] || 0]
+      end
+  end
+end
+# rubocop:enable Metrics/AbcSize
