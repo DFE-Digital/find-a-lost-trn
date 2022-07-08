3405c9b4f2ff533cfcc357d7a748743905b9cdee
Jake Benilov <benilov@gmail.com>
2022-07-08 23:13:02 +0100

    Performance page: show total requests as a card

--- app/controllers/performance_controller.rb
+++ app/controllers/performance_controller.rb
@@ -4,11 +4,13 @@ class PerformanceController < ApplicationController
   def index
     time_period = 1.week.ago.beginning_of_day..Time.zone.now
     @since_text = "over the last 7 days"
+    @short_since_text = "last 7 days"
 
     if params.key? :since_launch
       launch_date = Date.new(2022, 5, 4).beginning_of_day
       time_period = launch_date..Time.zone.now
       @since_text = "since launch"
+      @short_since_text = "since launch"
     end
 
     stats = PerformanceStats.new(time_period)
