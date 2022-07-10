3f0a2db90bd043e417cba41f6ad4b8b9aa8ca5eb
Jake Benilov <benilov@gmail.com>
2022-07-10 16:20:58 +0100

    Extract a performance table component from the performance page
    This makes it easier to reuse the same presentation logic for both
    a day rollup of requests and a month rollup.

---
+++ spec/components/trn_requests_performance_table_component_spec.rb
@@ -0,0 +1,46 @@
+require "rails_helper"
+
+RSpec.describe TrnRequestsPerformanceTableComponent, type: :component do
+  let(:total_grouped_requests) do
+    { total: 6, cnt_trn_found: 2, cnt_no_match: 2, cnt_did_not_finish: 2 }
+  end
+  let(:grouped_request_counts) do
+    [
+      [
+        "2 June",
+        { total: 3, cnt_trn_found: 1, cnt_no_match: 1, cnt_did_not_finish: 1 }
+      ],
+      [
+        "1 June",
+        { total: 3, cnt_trn_found: 1, cnt_no_match: 1, cnt_did_not_finish: 1 }
+      ]
+    ]
+  end
+
+  subject do
+    described_class.new(
+      grouped_request_counts:,
+      total_grouped_requests:,
+      since: "last 7 days"
+    )
+  end
+
+  it "renders a row per period" do
+    expect(rendered_result_text).to include(
+      "2 June\n    1 (33%)\n    1 (33%)\n    1 (33%)\n    3 requests"
+    )
+    expect(rendered_result_text).to include(
+      "1 June\n    1 (33%)\n    1 (33%)\n    1 (33%)\n    3 requests"
+    )
+  end
+
+  it "renders the totals row" do
+    expect(rendered_result_text).to include(
+      "Total (last 7 days)\n    2 (33%)\n    2 (33%)\n    2 (33%)\n    6 requests"
+    )
+  end
+
+  def rendered_result_text
+    render_inline(subject).text
+  end
+end
