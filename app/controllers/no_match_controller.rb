bcebe178773d49afaef172fb825a7d31397ceceb
Felix Clack <felixclack@gmail.com>
2022-07-14 13:12:05 +0100

    Queue a Zendesk check job for a no match
    When there isn't a match with the current details, we create a Zendesk
    ticket but don't queue a check job.

--- app/controllers/no_match_controller.rb
+++ app/controllers/no_match_controller.rb
@@ -28,5 +28,6 @@ class NoMatchController < ApplicationController
   def create_zendesk_ticket
     ZendeskService.create_ticket!(trn_request)
     TeacherMailer.information_received(trn_request).deliver_now
+    CheckZendeskTicketForTrnJob.set(wait: 2.days).perform_later(trn_request.id)
   end
 end
