2aedd625a6dcd1aad2d71108add49435df6176d6
Theodor Vararu <theo@vararu.org>
2022-07-28 14:34:31 +0100

    Prefer free text for provider autocomplete
    Disable the <select> element, leaving only the input to be considered by
    the server. Also disable the `autoselect` option as it's likely to be
    detrimental in this usecase.

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -2,4 +2,14 @@ import { initAll } from "govuk-frontend";
 import dfeAutocomplete from "dfe-autocomplete";
 
 initAll();
-dfeAutocomplete({});
+dfeAutocomplete({ autoselect: false });
+
+// In order to allow the autocomplete to work as a free text field, we need to
+// prevent the select element (that has the same name as the input field) from
+// being submitted.
+function disableDfeAutocompleteSelect() {
+  document.querySelector(
+    'select[name="itt_provider_form[itt_provider_name]"'
+  ).name = "";
+}
+disableDfeAutocompleteSelect();
