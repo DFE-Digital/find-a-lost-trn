3de9962fb134f6c434bdd0903a72bdee6265ec04
Theodor Vararu <theo@vararu.org>
2022-07-29 15:19:18 +0100

    Annotate models
    We haven't migrated in a while, so these files haven't received
    annotations.

--- app/models/trn_response.rb
+++ app/models/trn_response.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: trn_responses
+#
+#  id             :bigint           not null, primary key
+#  raw_result     :json
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  trn_request_id :bigint           not null
+#
+# Indexes
+#
+#  index_trn_responses_on_trn_request_id  (trn_request_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (trn_request_id => trn_requests.id)
+#
 class TrnResponse < ApplicationRecord
   belongs_to :trn_request
 end
--- app/models/validation_error.rb
+++ app/models/validation_error.rb
@@ -1,4 +1,25 @@
 # frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: validation_errors
+#
+#  id             :bigint           not null, primary key
+#  form_object    :string           not null
+#  messages       :jsonb            not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  trn_request_id :bigint           not null
+#
+# Indexes
+#
+#  index_validation_errors_on_messages        (messages) USING gin
+#  index_validation_errors_on_trn_request_id  (trn_request_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (trn_request_id => trn_requests.id)
+#
 class ValidationError < ApplicationRecord
   belongs_to :trn_request
 
