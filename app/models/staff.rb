48e5c6dc8893eb5c91ab4672bc00d95343146b7e
Theodor Vararu <theo@vararu.org>
2022-07-29 15:31:01 +0100

    Send Devise notifications with ActiveJob
    This allows us to send notifications in the background via ActiveJob
    rather than the default behaviour of sending emails synchronously.
    
    See https://github.com/DFE-Digital/apply-for-qualified-teacher-status/pull/143/commits/efa67f1048f9eb6daf7bac0c5962e56c3cfb7eb2

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -41,4 +41,8 @@ class Staff < ApplicationRecord
          :trackable,
          :timeoutable,
          :validatable
+
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
 end
