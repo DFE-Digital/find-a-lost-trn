3b2372260632b6d2951aeac2d5e11da0c6017639
Theodor Vararu <theo@vararu.org>
2022-07-29 15:13:34 +0100

    Add staff model
    Generated using:
    
    rails g devise staff
    
    Enable all relevant modules, as it's easier to do now than later.
    
    See https://github.com/DFE-Digital/apply-for-qualified-teacher-status/pull/141/commits/8fea045ef04d2b0829db71fdbb1984a85c1e151b

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_09_113250) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_29_140859) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,32 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_09_113250) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_features_on_name", unique: true
+  end
+
+  create_table "staff", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_staff_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_staff_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_staff_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_staff_on_unlock_token", unique: true
   end
 
   create_table "trn_requests", force: :cascade do |t|
