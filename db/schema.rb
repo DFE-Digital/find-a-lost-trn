bb261f4d56fce95c577c3feb74c64a04d74af5d1
Theodor Vararu <theo@vararu.org>
2022-07-29 15:34:18 +0100

    Make staff users invitable
    See https://github.com/DFE-Digital/apply-for-qualified-teacher-status/pull/143/commits/f60e9ffcec2706905a892936829486498db588c1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_29_140859) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_29_143332) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,8 +42,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_29_140859) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "invitation_token"
+    t.datetime "invitation_created_at", precision: nil
+    t.datetime "invitation_sent_at", precision: nil
+    t.datetime "invitation_accepted_at", precision: nil
+    t.integer "invitation_limit"
+    t.string "invited_by_type"
+    t.bigint "invited_by_id"
+    t.integer "invitations_count", default: 0
     t.index ["confirmation_token"], name: "index_staff_on_confirmation_token", unique: true
     t.index ["email"], name: "index_staff_on_email", unique: true
+    t.index ["invitation_token"], name: "index_staff_on_invitation_token", unique: true
+    t.index ["invited_by_id"], name: "index_staff_on_invited_by_id"
+    t.index ["invited_by_type", "invited_by_id"], name: "index_staff_on_invited_by"
     t.index ["reset_password_token"], name: "index_staff_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_staff_on_unlock_token", unique: true
   end
