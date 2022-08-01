1ecc5b90bd6c94a1281058a4a4a16d6c7ae683e7
Theodor Vararu <theo@vararu.org>
2022-08-01 13:11:02 +0100

    Use StaffHttpBasicAuthStrategy
    This uses the HTTP basic auth for the support interface, and Staff users
    if they exist.
    
    Based on
    https://github.com/DFE-Digital/apply-for-qualified-teacher-status/commit/62adeb4164c2a8bb99e573fb3a8e96c2c66459b0C

--- app/controllers/support_interface/support_interface_controller.rb
+++ app/controllers/support_interface/support_interface_controller.rb
@@ -3,7 +3,6 @@ module SupportInterface
   class SupportInterfaceController < ApplicationController
     layout "support_layout"
 
-    http_basic_authenticate_with name: ENV.fetch("SUPPORT_USERNAME", nil),
-                                 password: ENV.fetch("SUPPORT_PASSWORD", nil)
+    before_action :authenticate_staff!
   end
 end
