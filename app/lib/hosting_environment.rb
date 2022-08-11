c6aae532dc912b152a99bea3ace6cc59f636188b
Theodor Vararu <theo@vararu.org>
2022-08-11 10:19:55 +0100

    Add host to hosting_environment
    This was missed in https://github.com/DFE-Digital/find-a-lost-trn/pull/378

--- app/lib/hosting_environment.rb
+++ app/lib/hosting_environment.rb
@@ -3,6 +3,10 @@ module HostingEnvironment
   TEST_ENVIRONMENTS = %w[local test development].freeze
   PRODUCTION_URL = "https://find-a-lost-trn.education.gov.uk/"
 
+  def self.host
+    ENV.fetch("HOSTING_DOMAIN")
+  end
+
   def self.environment_name
     ENV.fetch("HOSTING_ENVIRONMENT_NAME", "unknown-environment")
   end
