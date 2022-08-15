a9af4c297efafeb28f087e0715d2359ee1c9d6f4
Theodor Vararu <theo@vararu.org>
2022-08-15 09:38:39 +0100

    Run db:migrate:data_migrate
    It seems like the schema needs to be updated as a result.

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_05_095822) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "data_migrations", primary_key: "version", id: :string, force: :cascade do |t|
+  end
+
   create_table "features", force: :cascade do |t|
     t.string "name", null: false
     t.boolean "active", default: false, null: false
