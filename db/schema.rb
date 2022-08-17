76480afa2471e243513517ab437769b619519d25
Theodor Vararu <theo@vararu.org>
2022-08-17 15:36:08 +0100

    Add DeletedZendeskTicket model
    This will be used to track any Zendesk tickets that we delete, and
    retain some useful metadata, but no PII.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_17_105900) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_17_143001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_17_105900) do
   end
 
   create_table "data_migrations", primary_key: "version", id: :string, force: :cascade do |t|
+  end
+
+  create_table "deleted_zendesk_tickets", force: :cascade do |t|
+    t.datetime "received_at", null: false
+    t.datetime "closed_at", null: false
+    t.integer "ticket_id", null: false
+    t.string "enquiry_type", null: false
+    t.string "no_action_required", null: false
+    t.string "group_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "features", force: :cascade do |t|
