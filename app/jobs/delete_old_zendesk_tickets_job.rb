84dfcddf4d5bad8b5bdf0be0fd5753317d3da436
Theodor Vararu <theo@vararu.org>
2022-08-17 16:07:27 +0100

    Create DeletedZendeskTicket entries when deleting zendesk tickets

--- app/jobs/delete_old_zendesk_tickets_job.rb
+++ app/jobs/delete_old_zendesk_tickets_job.rb
@@ -3,6 +3,8 @@ class DeleteOldZendeskTicketsJob < ApplicationJob
     tickets = ZendeskService.find_closed_tickets_from_6_months_ago
     return if tickets.size.zero?
 
+    tickets.each { |ticket| DeletedZendeskTicket.new.from(ticket).save! }
+
     ids = tickets.map(&:id)
     ZendeskService.destroy_tickets!(ids)
   end
