a707ac109d5beb184e1c9c07726b8c324bef97d1
Theodor Vararu <theo@vararu.org>
2022-08-18 11:50:22 +0100

    Raise an error if DeleteOldZendeskTicketsJob is called with >100 tickets

--- app/jobs/delete_old_zendesk_tickets_job.rb
+++ app/jobs/delete_old_zendesk_tickets_job.rb
@@ -2,6 +2,11 @@ class DeleteOldZendeskTicketsJob < ApplicationJob
   def perform
     tickets = ZendeskService.find_closed_tickets_from_6_months_ago
     return if tickets.size.zero?
+    if tickets.size >= 100
+      raise "More than 100 tickets to delete were found in Zendesk. " \
+              "The DeleteOldZendeskTicketsJob doesn't handle pagination. " \
+              "If you're seeing this, we need to build pagination."
+    end
 
     tickets.each do |ticket|
       ZendeskDeleteRequest
