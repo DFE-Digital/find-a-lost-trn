dafb3fe257f0661a9f884d291555dfcd8aa531a5
Theodor Vararu <theo@vararu.org>
2022-08-18 11:50:22 +0100

    Raise an error if DeleteOldZendeskTicketsJob is called with >100 tickets

--- spec/jobs/delete_old_zendesk_tickets_job_spec.rb
+++ spec/jobs/delete_old_zendesk_tickets_job_spec.rb
@@ -4,39 +4,43 @@ RSpec.describe DeleteOldZendeskTicketsJob, type: :job do
   describe "#perform" do
     subject(:perform) { described_class.new.perform }
 
+    let(:tickets) do
+      [
+        ZendeskAPI::Ticket.new(
+          GDS_ZENDESK_CLIENT,
+          id: 42,
+          custom_fields: [
+            { id: 4_419_328_659_089, value: "Foo" },
+            { id: 4_562_126_876_049, value: "Bar" }
+          ],
+          group: {
+            name: "Some group"
+          },
+          created_at: 6.months.ago + 7.days,
+          updated_at: 6.months.ago + 1.day
+        ),
+        ZendeskAPI::Ticket.new(
+          GDS_ZENDESK_CLIENT,
+          id: 13,
+          custom_fields: [
+            { id: 4_419_328_659_089, value: "Foo" },
+            { id: 4_562_126_876_049, value: "Bar" }
+          ],
+          group: {
+            name: "Some other group"
+          },
+          created_at: 6.months.ago + 8.days,
+          updated_at: 6.months.ago + 2.days
+        )
+      ]
+    end
+
+    let(:returned_tickets) { tickets }
+
     before do
       allow(ZendeskService).to receive(
         :find_closed_tickets_from_6_months_ago
-      ).and_return(
-        [
-          ZendeskAPI::Ticket.new(
-            GDS_ZENDESK_CLIENT,
-            id: 42,
-            custom_fields: [
-              { id: 4_419_328_659_089, value: "Foo" },
-              { id: 4_562_126_876_049, value: "Bar" }
-            ],
-            group: {
-              name: "Some group"
-            },
-            created_at: 6.months.ago + 7.days,
-            updated_at: 6.months.ago + 1.day
-          ),
-          ZendeskAPI::Ticket.new(
-            GDS_ZENDESK_CLIENT,
-            id: 13,
-            custom_fields: [
-              { id: 4_419_328_659_089, value: "Foo" },
-              { id: 4_562_126_876_049, value: "Bar" }
-            ],
-            group: {
-              name: "Some other group"
-            },
-            created_at: 6.months.ago + 8.days,
-            updated_at: 6.months.ago + 2.days
-          )
-        ]
-      )
+      ).and_return(returned_tickets)
       allow(ZendeskService).to receive(:destroy_tickets!)
     end
 
@@ -52,6 +56,13 @@ RSpec.describe DeleteOldZendeskTicketsJob, type: :job do
 
     it "creates deleted ticket entries" do
       expect { perform }.to change { ZendeskDeleteRequest.count }.by 2
+    end
+
+    context "with 100 or more tickets" do
+      let(:returned_tickets) { tickets * 50 }
+      it "raises an error" do
+        expect { perform }.to raise_error RuntimeError
+      end
     end
   end
 end
