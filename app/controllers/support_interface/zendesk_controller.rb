0a9e1f1fb2691486d3fce500e55604d9c05ef22e
Theodor Vararu <theo@vararu.org>
2022-08-22 15:13:49 +0100

    Add Zendesk ticket deletion routes
    This adds a button that users can click in support to delete all
    outstanding Zendesk tickets.
    
    The actual job is not added yet, and this needs tests.

--- app/controllers/support_interface/zendesk_controller.rb
+++ app/controllers/support_interface/zendesk_controller.rb
@@ -10,5 +10,35 @@ module SupportInterface
             end
           end
     end
+
+    def confirm_deletion
+      @ticket_count = ZendeskService.find_closed_tickets_from_6_months_ago.count
+      @delete_form = ZendeskTicketDeletionForm.new
+    end
+
+    def destroy
+      @ticket_count = ZendeskService.find_closed_tickets_from_6_months_ago.count
+      @delete_form =
+        ZendeskTicketDeletionForm.new(
+          actual_number_of_tickets: @ticket_count.to_s,
+          number_of_tickets: destroy_params
+        )
+
+      if @delete_form.valid?
+        flash[:success] = "Successfully deleted #{@ticket_count} tickets"
+        redirect_to support_interface_zendesk_path
+      else
+        render :confirm_deletion
+      end
+    end
+
+    private
+
+    def destroy_params
+      params
+        .require(:support_interface_zendesk_ticket_deletion_form)
+        .fetch(:number_of_tickets)
+        .strip
+    end
   end
 end
---
+++ app/forms/support_interface/zendesk_ticket_deletion_form.rb
@@ -0,0 +1,12 @@
+module SupportInterface
+  class ZendeskTicketDeletionForm
+    include ActiveModel::Model
+
+    attr_accessor :actual_number_of_tickets, :number_of_tickets
+
+    validates :number_of_tickets,
+              comparison: {
+                equal_to: :actual_number_of_tickets
+              }
+  end
+end
