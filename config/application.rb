e130d30525956f45c0fc66dabd57446bb5c4d23c
Theodor Vararu <theo@vararu.org>
2022-08-24 11:28:04 +0100

    Assign to config.console1984 instead of replacing the hash
    Assigning to an existing hash doesn't replace existing default keys.

--- config/application.rb
+++ config/application.rb
@@ -42,7 +42,7 @@ module FindALostTrn
     config.active_job.queue_adapter = :sidekiq
 
     config.exceptions_app = routes
-    config.console1984 = { ask_for_username_if_empty: true }
+    config.console1984.ask_for_username_if_empty = true
     config.audits1984 = {
       auditor_class: "Staff",
       base_controller_class: "SupportInterface::SupportInterfaceController"
