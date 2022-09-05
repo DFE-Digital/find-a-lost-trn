04ab45ac7c5652ab1cb46efa41a8517a666e7427
Voke Ransom Anighoro <ransom4real@gmail.com>
2022-09-05 13:08:57 +0100

    Make DeleteOldZendeskTicketsJob work with >100 tickets
    We built this job but purposefully added in a guard clause / raise to break if it's run with more than 100 tickets.
    
    The ZendeskService.find_closed_tickets_from_6_months_ago can return a maximum of 100 tickets and is a paginated request. destroy_tickets! can only operate with a maximum of 100 tickets.
    
    Run the job recursively with more than 100 tickets

--- app/jobs/delete_old_zendesk_tickets_job.rb
+++ app/jobs/delete_old_zendesk_tickets_job.rb
@@ -3,9 +3,7 @@ class DeleteOldZendeskTicketsJob < ApplicationJob
     tickets = ZendeskService.find_closed_tickets_from_6_months_ago
     return if tickets.size.zero?
     if tickets.size >= 100
-      raise "More than 100 tickets to delete were found in Zendesk. " \
-              "The DeleteOldZendeskTicketsJob doesn't handle pagination. " \
-              "If you're seeing this, we need to build pagination."
+      DeleteOldZendeskTicketsJob.set(wait: 5.minutes).perform_later
     end
 
     tickets.each do |ticket|
