65586e2d79d74662886ac56ec1986be90d295cdd
Theodor Vararu <theo@vararu.org>
2022-09-05 17:07:44 +0100

    Remove caching in Zendesk controller index
    Now that performance isn't bottlenecked anymore, we can remove caching.

--- app/controllers/support_interface/zendesk_controller.rb
+++ app/controllers/support_interface/zendesk_controller.rb
@@ -2,13 +2,9 @@ module SupportInterface
   class ZendeskController < SupportInterfaceController
     def index
       @zendesk_tickets =
-        Rails
-          .cache
-          .fetch("zendesk_tickets", expires_in: 1.hour) do
-            ZendeskService.find_closed_tickets_from_6_months_ago.map do |t|
-              ZendeskDeleteRequest.new.from(t)
-            end
-          end
+        ZendeskService.find_closed_tickets_from_6_months_ago.map do |t|
+          ZendeskDeleteRequest.new.from(t)
+        end
       @zendesk_delete_requests = ZendeskDeleteRequest.all
     end
 
