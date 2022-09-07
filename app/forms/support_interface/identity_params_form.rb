3f220d98843d39ed9c4294a0cdf667d351e290b4
Theodor Vararu <theo@vararu.org>
2022-09-07 16:45:35 +0100

    Stub client_url in simulated Identity journeys
    This parameter was accidentally omitted in the initial implementation.

--- app/forms/support_interface/identity_params_form.rb
+++ app/forms/support_interface/identity_params_form.rb
@@ -2,6 +2,6 @@ module SupportInterface
   class IdentityParamsForm
     include ActiveModel::Model
 
-    attr_accessor :client_title, :email, :journey_id, :redirect_uri
+    attr_accessor :client_title, :email, :journey_id, :redirect_uri, :client_url
   end
 end
