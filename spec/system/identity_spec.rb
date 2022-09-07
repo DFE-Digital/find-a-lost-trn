7deb7a1041a12cc04676d758330e931f96594356
Theodor Vararu <theo@vararu.org>
2022-09-07 15:17:53 +0100

    Update Identity spec
    Switch from request based specs to regular Capybara `visit` specs.

--- spec/system/identity_spec.rb
+++ spec/system/identity_spec.rb
@@ -4,44 +4,50 @@ require "rails_helper"
 RSpec.describe "Identity", type: :system do
   before do
     given_the_service_is_open
+    given_i_am_authorized_as_a_support_user
     given_the_identity_endpoint_is_open
   end
 
   after { deactivate_feature_flags }
 
-  it "verifies the parameters" do
-    when_i_access_the_identity_endpoint_with_parameters
+  it "redirects to name page when parameters are verified correctly" do
+    when_i_access_the_identity_endpoint
     then_i_see_the_name_page
   end
 
   it "does not verify with modified parameters" do
+    given_the_service_is_open
     when_i_access_the_identity_endpoint_with_modified_parameters_it_raises_an_error
   end
 
   it "does not allow user to change their email" do
-    when_i_access_the_identity_endpoint_with_parameters
+    when_i_access_the_identity_endpoint
     then_i_see_the_name_page
+
     when_i_try_to_go_to_the_email_page
     then_i_see_the_name_page
   end
 
   it "does not ask for an email address", vcr: true do
-    when_i_access_the_identity_endpoint_with_parameters
+    when_i_access_the_identity_endpoint
     then_i_see_the_name_page
+
     when_i_complete_and_submit_the_name_form
     then_i_see_the_date_of_birth_page
+
     when_i_complete_and_submit_my_date_of_birth
     then_i_see_the_check_answers_page
   end
 
   context "when sending the trn to the Identity API" do
     it "redirects back to Get An Identity", vcr: true do
-      when_i_access_the_identity_endpoint_with_parameters
+      when_i_access_the_identity_endpoint
       and_i_complete_and_submit_the_name_form
       and_i_complete_and_submit_my_date_of_birth
       then_i_see_the_check_answers_page
+
       when_i_press_the_continue_button
-      then_i_am_redirected_back_to_get_an_identity
+      then_i_am_redirected_to_the_callback
     end
 
     context "when there is no trn match", vcr: true do
@@ -49,25 +55,23 @@ RSpec.describe "Identity", type: :system do
         allow(DqtApi).to receive(:find_trn!).and_raise(DqtApi::NoResults)
       end
 
-      it "does not ask for an email address" do
-        when_i_access_the_identity_endpoint_with_parameters
-        then_i_see_the_name_page
-        when_i_complete_and_submit_the_name_form
-        then_i_see_the_date_of_birth_page
-        when_i_complete_and_submit_my_date_of_birth
-        then_i_see_the_have_ni_number_page
-        when_i_have_a_ni_number
-        then_i_see_the_ni_number_page
-        when_i_complete_and_submit_my_ni_number
+      it "asks for the TRN" do
+        when_i_access_the_identity_endpoint
+        and_i_complete_and_submit_the_name_form
+        and_i_complete_and_submit_my_date_of_birth
+        and_i_have_a_ni_number
+        and_i_complete_and_submit_my_ni_number
         then_i_see_the_ask_trn_page
+
         when_i_do_not_know_the_trn
         then_i_see_the_have_awarded_qts_page
+
         when_i_have_not_been_awarded_qts
         then_i_see_the_check_answers_page
       end
 
       it "sends a blank trn to Get an Identity and redirects back Get An Identity" do
-        when_i_access_the_identity_endpoint_with_parameters
+        when_i_access_the_identity_endpoint
         and_i_complete_and_submit_the_name_form
         and_i_complete_and_submit_my_date_of_birth
         and_i_have_a_ni_number
@@ -75,14 +79,16 @@ RSpec.describe "Identity", type: :system do
         and_i_do_not_know_the_trn
         and_i_have_not_been_awarded_qts
         then_i_see_the_check_answers_page
+
         when_i_press_the_continue_button
         then_i_see_the_no_match_page
+
         when_i_submit_anyway
-        then_i_am_redirected_back_to_get_an_identity
+        then_i_am_redirected_to_the_callback
       end
 
       it "has the correct no match page copy for Get an Identity journey" do
-        when_i_access_the_identity_endpoint_with_parameters
+        when_i_access_the_identity_endpoint
         and_i_complete_and_submit_the_name_form
         and_i_complete_and_submit_my_date_of_birth
         and_i_have_a_ni_number
@@ -90,9 +96,10 @@ RSpec.describe "Identity", type: :system do
         and_i_do_not_know_the_trn
         and_i_have_not_been_awarded_qts
         then_i_see_the_check_answers_page
+
         when_i_press_the_continue_button
         then_i_see_the_no_match_page
-        and_i_see_the_correct_no_match_copy
+        and_i_see_the_correct_no_match_content
       end
     end
 
@@ -105,14 +112,11 @@ RSpec.describe "Identity", type: :system do
       after { FeatureFlag.deactivate(:identity_api_always_timeout) }
 
       it "redirects to the error page", vcr: true do
-        when_i_access_the_identity_endpoint_with_parameters
+        when_i_access_the_identity_endpoint
         and_i_complete_and_submit_the_name_form
         and_i_complete_and_submit_my_date_of_birth
-        and_i_have_a_ni_number
-        and_i_complete_and_submit_my_ni_number
-        and_i_have_not_been_awarded_qts
         then_i_see_the_check_answers_page
-        and_i_see_a_continue_button
+
         when_i_press_the_continue_button
         then_sentry_receives_a_warning_about_the_failure
         and_i_am_redirected_to_the_error_page
@@ -122,7 +126,7 @@ RSpec.describe "Identity", type: :system do
 
   context "ask for TRN page" do
     it "matches if they don't match on other criteria", vcr: true do
-      when_i_access_the_identity_endpoint_with_parameters
+      when_i_access_the_identity_endpoint
       then_i_see_the_name_page
 
       when_i_complete_and_submit_the_name_form
@@ -139,7 +143,7 @@ RSpec.describe "Identity", type: :system do
     end
 
     it "does not ask if they match on other criteria", vcr: true do
-      when_i_access_the_identity_endpoint_with_parameters
+      when_i_access_the_identity_endpoint
       then_i_see_the_name_page
 
       when_i_complete_and_submit_the_name_form
@@ -150,7 +154,7 @@ RSpec.describe "Identity", type: :system do
     end
 
     it "asks for QTS if they do not match on any criteria", vcr: true do
-      when_i_access_the_identity_endpoint_with_parameters
+      when_i_access_the_identity_endpoint
       then_i_see_the_name_page
 
       when_i_complete_and_submit_the_name_form
@@ -169,31 +173,18 @@ RSpec.describe "Identity", type: :system do
 
   context "custom client_title from get an identity" do
     it "displays the client_title from get an identity journey" do
-      when_i_access_the_identity_endpoint_with_parameters
+      when_i_access_the_identity_endpoint
       then_i_see_the_name_page
       then_i_should_see_the_client_title_from_get_an_identity
     end
   end
 
-  it "renders a link to the calling service" do
-    when_i_access_the_identity_endpoint_with_parameters
-    then_i_see_the_name_page
-    and_it_links_to_the_calling_service
-  end
-
   private
 
-  def when_i_access_the_identity_endpoint_with_parameters
-    params = {
-      client_title: "The Client Title",
-      client_url: "https://clienturi/",
-      email: "kevin.e@example.com",
-      journey_id: "9ddccb62-ec13-4ea7-a163-c058a19b8222",
-      redirect_uri: "https://authserveruri/",
-      sig: "2b92f4e7c5d22e3627f477ade69fd0a8f2c0f948c33572d53ebaece8534f858b"
-    }
-
-    post identity_path, params:
+  def when_i_access_the_identity_endpoint
+    visit support_interface_identity_path
+    click_on "Continue"
+    click_on "Submit"
   end
 
   def when_i_access_the_identity_endpoint_with_modified_parameters_it_raises_an_error
@@ -211,122 +202,87 @@ RSpec.describe "Identity", type: :system do
   end
 
   def when_i_complete_and_submit_the_name_form
-    post "/name",
-         params: {
-           name_form: {
-             first_name: "Kevin",
-             last_name: "E",
-             name_changed: false
-           }
-         }
+    fill_in "First name", with: "Kevin"
+    fill_in "Last name", with: "E"
+    choose "No, I’ve not changed my name", visible: false
+    click_on "Continue"
   end
   alias_method :and_i_complete_and_submit_the_name_form,
                :when_i_complete_and_submit_the_name_form
 
   def when_i_complete_and_submit_my_date_of_birth
-    post "/date-of-birth",
-         params: {
-           date_of_birth_form: {
-             "date_of_birth(3i)" => "1",
-             "date_of_birth(2i)" => "1",
-             "date_of_birth(1i)" => "1990"
-           }
-         }
+    fill_in "Day", with: "01"
+    fill_in "Month", with: "01"
+    fill_in "Year", with: "1990"
+    click_on "Continue"
   end
   alias_method :and_i_complete_and_submit_my_date_of_birth,
                :when_i_complete_and_submit_my_date_of_birth
 
   def when_i_complete_and_submit_my_wrong_date_of_birth
-    post "/date-of-birth",
-         params: {
-           date_of_birth_form: {
-             "date_of_birth(3i)" => "1",
-             "date_of_birth(2i)" => "1",
-             "date_of_birth(1i)" => "1999"
-           }
-         }
+    fill_in "Day", with: "01"
+    fill_in "Month", with: "01"
+    fill_in "Year", with: "1999"
+    click_on "Continue"
   end
 
   def when_i_have_a_ni_number
-    post "/have-ni-number",
-         params: {
-           has_ni_number_form: {
-             has_ni_number: true
-           }
-         }
+    choose "Yes", visible: false
+    click_on "Continue"
   end
   alias_method :and_i_have_a_ni_number, :when_i_have_a_ni_number
 
   def when_i_dont_have_a_ni_number
-    post "/have-ni-number",
-         params: {
-           has_ni_number_form: {
-             has_ni_number: false
-           }
-         }
+    choose "No", visible: false
+    click_on "Continue"
   end
 
   def when_i_complete_and_submit_my_ni_number
-    post "/ni-number",
-         params: {
-           ni_number_form: {
-             ni_number: "AA123456A"
-           },
-           submit: "submit"
-         }
+    fill_in "What is your National Insurance number?", with: "AA123456A"
+    click_on "Continue"
   end
   alias_method :and_i_complete_and_submit_my_ni_number,
                :when_i_complete_and_submit_my_ni_number
 
   def when_i_do_not_know_the_trn
-    post "/ask-trn", params: { ask_trn_form: { do_you_know_your_trn: false } }
+    choose "No, I need to continue without my TRN", visible: false
+    click_on "Continue"
   end
   alias_method :and_i_do_not_know_the_trn, :when_i_do_not_know_the_trn
 
   def when_i_submit_my_trn
-    post "/ask-trn",
-         params: {
-           ask_trn_form: {
-             do_you_know_your_trn: true,
-             trn_from_user: "2921020"
-           }
-         }
+    choose "Yes, I know my TRN", visible: false
+    fill_in "What is your TRN?", with: "2921020"
+    click_on "Continue"
   end
 
   def when_i_submit_my_wrong_trn
-    post "/ask-trn",
-         params: {
-           ask_trn_form: {
-             do_you_know_your_trn: true,
-             trn_from_user: "1234567"
-           }
-         }
+    choose "Yes, I know my TRN", visible: false
+    fill_in "What is your TRN?", with: "1234567"
+    click_on "Continue"
   end
 
   def when_i_have_not_been_awarded_qts
-    post "/awarded-qts", params: { awarded_qts_form: { awarded_qts: false } }
+    choose "No", visible: false
+    click_on "Continue"
   end
   alias_method :and_i_have_not_been_awarded_qts,
                :when_i_have_not_been_awarded_qts
 
   def then_i_see_the_check_answers_page
-    expect(response).to redirect_to("/check-answers")
-  end
-
-  def then_i_see_the_ask_for_trn_page
-    expect(response).to redirect_to("/ask-trn")
+    expect(page).to have_content("Check your answers")
   end
 
   def then_i_see_the_name_page
-    expect(response).to redirect_to("/name")
+    expect(page).to have_content("Your name")
   end
 
   def then_i_see_the_date_of_birth_page
-    expect(response).to redirect_to("/date-of-birth")
+    expect(page).to have_content("Your date of birth")
   end
 
   def then_i_see_the_have_ni_number_page
-    expect(response).to redirect_to("/have-ni-number")
+    expect(page).to have_content("Do you have a National Insurance number")
   end
 
   def then_i_see_the_ni_number_page
@@ -334,32 +290,31 @@ RSpec.describe "Identity", type: :system do
   end
 
   def then_i_see_the_ask_trn_page
-    expect(response).to redirect_to("/ask-trn")
+    expect(page).to have_content("Do you know your teacher reference number")
   end
 
   def then_i_see_the_have_awarded_qts_page
-    expect(response).to redirect_to("/awarded-qts")
-  end
-
-  def then_i_see_the_have_itt_provider_page
-    expect(response).to redirect_to("/itt-provider")
+    expect(page).to have_content(
+      "Have you been awarded qualified teacher status"
+    )
   end
 
   def when_i_try_to_go_to_the_email_page
-    get email_path
+    visit email_path
   end
 
   def when_i_press_the_continue_button
-    put "/trn-request", params: { trn_request: { answers_checked: true } }
+    click_on "Continue"
   end
-  alias_method :when_i_submit_anyway, :when_i_press_the_continue_button
 
   def then_i_see_the_no_match_page
-    expect(response).to redirect_to("/no-match")
+    expect(page).to have_content("We could not match")
   end
 
-  def then_i_am_redirected_back_to_get_an_identity
-    expect(response).to redirect_to("https://authserveruri/")
+  def then_i_am_redirected_to_the_callback
+    expect(page).to have_content(
+      "You have completed a simulated Identity journey"
+    )
   end
 
   def then_sentry_receives_a_warning_about_the_failure
@@ -367,14 +322,10 @@ RSpec.describe "Identity", type: :system do
   end
 
   def then_i_should_see_the_client_title_from_get_an_identity
-    follow_redirect!
-    page = response.parsed_body
-    expect(page).to have_content("The Client Title")
+    expect(page).to have_content("")
   end
 
-  def and_i_see_the_correct_no_match_copy
-    follow_redirect!
-    page = response.parsed_body
+  def and_i_see_the_correct_no_match_content
     expect(page).to have_content(
       "We could not match your answers with our records"
     )
@@ -384,34 +335,27 @@ RSpec.describe "Identity", type: :system do
   end
 
   def and_i_am_redirected_to_the_error_page
-    expect(response.status).to be(500)
+    expect(page).to have_content("Sorry")
   end
 
   def when_i_submit_anyway
-    post "/no-match", params: { no_match_form: { try_again: false } }
-  end
-
-  def deactivate_feature_flags
-    FeatureFlag.deactivate(:service_open)
-    FeatureFlag.deactivate(:identity_open)
+    choose "No, submit these details, they are correct", visible: false
+    click_on "Continue"
   end
 
   def given_the_service_is_open
     FeatureFlag.activate(:service_open)
   end
 
+  def deactivate_feature_flags
+    FeatureFlag.deactivate(:identity_open)
+  end
+
+  def given_i_am_authorized_as_a_support_user
+    page.driver.basic_authorize("test", "test")
+  end
+
   def given_the_identity_endpoint_is_open
     FeatureFlag.activate(:identity_open)
-  end
-
-  def and_i_see_a_continue_button
-    follow_redirect!
-    expect(response.parsed_body).to have_button("Continue", visible: false)
-  end
-
-  def and_it_links_to_the_calling_service
-    follow_redirect!
-    page = response.parsed_body
-    expect(page).to have_link(href: "https://clienturi/")
   end
 end
