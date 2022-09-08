836b21f851055a528107609b16f1eaf9ed6e9f97
Theodor Vararu <theo@vararu.org>
2022-09-08 10:47:40 +0100

    Add DownloadHelpers for system specs
    Cuprite doesn't handle downloads very elegantly out of the box. It
    doesn't offer a documented way to wait for a `network_traffic.last` to
    complete.
    
    This adds a `DownloadHelpers` module taken from a GitHub comment that
    effectively sleeps in a loop until downloads get written to disk,
    followed by reading the file and providing the content for assertions.

---
+++ spec/support/download_helpers.rb
@@ -0,0 +1,45 @@
+module System
+  module DownloadHelpers
+    TIMEOUT = 10
+    PATH = Rails.root.join("tmp/capybara")
+
+    def downloads
+      Dir[PATH.join("*")].reject { |f| File.directory?(f) }
+    end
+
+    def download
+      downloads.first
+    end
+
+    def download_content
+      wait_for_download
+      File.read(download)
+    end
+
+    def wait_for_download
+      Timeout.timeout(TIMEOUT, Timeout::Error) { sleep 0.1 until downloaded? }
+    end
+
+    def downloaded?
+      !downloading? && downloads.any?
+    end
+
+    def downloading?
+      downloads.grep(/\.crdownload$/).any?
+    end
+
+    def clear_downloads
+      FileUtils.rm_f(downloads)
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include System::DownloadHelpers, type: :system
+
+  config.around(:each, type: :system) do |example|
+    clear_downloads
+    example.run
+    clear_downloads
+  end
+end
