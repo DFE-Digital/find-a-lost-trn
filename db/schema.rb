9e2c5cba200ac0b636724e8a8780efb4d9753a5e
Jake Benilov <benilov@gmail.com>
2022-09-16 14:01:12 +0100

    Add new AccountUnlockEvent to track unlocks

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_124341) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_16_125240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "account_unlock_events", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "audits1984_audits", force: :cascade do |t|
     t.integer "status", default: 0, null: false
