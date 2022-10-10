92db1934e0dd7ea6ca9f5d897363dd1a4f28f13d
Malcolm Baig <git@baig.org>
2022-10-10 10:44:34 +0100

    Specify after sign out path for staff users
    Currently it defaults to the application root, which isn't appropriate
    for staff users. Instead, redirect them to staff sign in.

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -18,6 +18,10 @@ class Staff::SessionsController < Devise::SessionsController
   #   super
   # end
 
+  def after_sign_out_path_for(_resource_or_scope)
+    new_staff_session_path
+  end
+
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
