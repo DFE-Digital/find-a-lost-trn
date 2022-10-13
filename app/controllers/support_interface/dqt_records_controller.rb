1de656bc1a579b50da7a568010ed9858f73f8fa6
Theodor Vararu <theo@vararu.org>
2022-10-13 16:06:43 +0200

    Fix inheritance in DqtRecordsController
    It should inherit from SupportInterfaceController.

--- app/controllers/support_interface/dqt_records_controller.rb
+++ app/controllers/support_interface/dqt_records_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module SupportInterface
-  class DqtRecordsController < ApplicationController
+  class DqtRecordsController < SupportInterfaceController
     include ConsumesIdentityUsersApi
 
     def edit
