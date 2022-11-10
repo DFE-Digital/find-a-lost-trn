310ce873bf5b83170120f665258699be49bec0f5
Theodor Vararu <theo@vararu.org>
2022-11-10 13:56:31 +0000

    Add Identity accessibility statement

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,6 +3,13 @@
 class StaticController < ApplicationController
   layout "two_thirds"
 
+  def accessibility
+    case session[:identity_client_id]
+    when "register-for-npq"
+      render "register_for_npq_accessibility"
+    end
+  end
+
   def privacy
     case session[:identity_client_id]
     when "register-for-npq"
