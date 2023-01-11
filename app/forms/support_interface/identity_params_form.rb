da82ec50bf75d02b9eb2f0d77e355eff25b9a947
Felix Clack <felixclack@gmail.com>
2023-01-11 14:18:05 +0000

    Accept session_id from Get an Identity
    The Get an Identity service will soon be sending a new param in the
    hand-off named `session_id`.
    
    This should be passed back whenever we redirect the user back to the
    calling service.
    
    There is no requirement to persist the value anywhere.

--- app/forms/support_interface/identity_params_form.rb
+++ app/forms/support_interface/identity_params_form.rb
@@ -2,12 +2,13 @@ module SupportInterface
   class IdentityParamsForm
     include ActiveModel::Model
 
-    attr_accessor :client_title,
-                  :client_id,
+    attr_accessor :client_id,
+                  :client_title,
+                  :client_url,
                   :email,
                   :journey_id,
+                  :previous_url,
                   :redirect_url,
-                  :client_url,
-                  :previous_url
+                  :session_id
   end
 end
