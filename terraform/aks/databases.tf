c6a563b4faad0af3266a573932edceee771b7a14
John Ake <johnake@hotmail.com>
2023-08-14 14:25:51 +0100

    Add Redis module

--- terraform/aks/databases.tf
+++ terraform/aks/databases.tf
@@ -16,3 +16,19 @@ module "postgres" {
   server_version            = "14"
 
 }
+
+module "redis" {
+  source = "./vendor/modules/aks//aks/redis"
+
+  namespace             = var.namespace
+  environment           = local.environment
+  azure_resource_prefix = var.azure_resource_prefix
+  service_name          = local.service_name
+  service_short         = var.service_short
+  config_short          = var.config_short
+
+  cluster_configuration_map = module.cluster_data.configuration_map
+
+  use_azure               = var.deploy_azure_backing_services
+  azure_enable_monitoring = var.enable_monitoring
+}
