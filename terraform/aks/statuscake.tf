3d6adbd82d1155282bb7eeee324a27011ceb2e10
John Ake <johnake@hotmail.com>
2023-10-17 12:02:47 +0100

    Add statuscake monitoring for preprod and prod

---
+++ terraform/aks/statuscake.tf
@@ -0,0 +1,29 @@
+module "statuscake" {
+  for_each = var.statuscake_alerts
+
+  source = "./vendor/modules/aks//monitoring/statuscake"
+
+  uptime_urls    = each.value.website_url
+  contact_groups = each.value.contact_group
+}
+
+resource "statuscake_ssl_check" "domain-alert" {
+  for_each = var.statuscake_alerts
+
+  check_interval   = 3600 # Check once per hour
+  contact_groups   = each.value.contact_group
+  follow_redirects = true
+
+  alert_config {
+    alert_at = [3, 7, 30] # Alert 1 month, 1 week then 3 days before expiration
+
+    on_reminder = true
+    on_expiry   = true
+    on_broken   = true
+    on_mixed    = true
+  }
+
+  monitored_resource {
+    address = each.value.ssl_domain
+  }
+}
