264dab9fd5c03fc16f95a50fb6a4bce73fe38130
Steve Laing <steve.laing@gmail.com>
2024-02-01 13:23:58 +0000

    Fix Rails >=7.1 / RedCarpet incompatibility bug

--- config/initializers/govuk_markdown.rb
+++ config/initializers/govuk_markdown.rb
@@ -2,8 +2,15 @@
 
 class MarkdownTemplate
   def self.call(template, source)
+    source ||= template.source
+
+    # Rails >= 7.1 does not work with the Redcarpet markdown renderer.
+    # There is an argument mismatch where a string is expected but an OutputBuffer is provided.
+    # This is a workaround to convert the buffer to a string.
     erb_handler = ActionView::Template.registered_template_handler(:erb)
-    compiled_source = erb_handler.call(template, source)
+    erb_handler.call(template, source)
+    compiled_source = ActionView::OutputBuffer.new( erb_handler.call( template, source ) )
+    compiled_source << '.to_s'
     "GovukMarkdown.render(begin;#{compiled_source};end).html_safe"
   end
 end
