0abfd83b6e2a882ea411921b981fe4eb87e74ad7
Malcolm Baig <git@baig.org>
2024-05-29 12:40:19 +0000

    Fix linting error

--- config/initializers/semantic_logger.rb
+++ config/initializers/semantic_logger.rb
@@ -5,5 +5,6 @@ Rails.application.configure do
   config.log_tags = [:request_id]         # Prepend all log lines with the following tags
 end
 
-SemanticLogger.add_appender(io: STDOUT, level: Rails.application.config.log_level, formatter: Rails.application.config.log_format)
+SemanticLogger.add_appender(io: $stdout, level: Rails.application.config.log_level, 
+formatter: Rails.application.config.log_format)
 Rails.application.config.logger.info('Application logging to STDOUT')
