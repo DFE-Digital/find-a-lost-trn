0e9880cf70e0b5040a93615f9ab281373d0be9b6
Naomi Lockhart <naomi@lockyy.com>
2026-08-11 15:22:52 +0100

    Revert "Re-dump db/schema.rb in the Rails 8.1 format"
    This reverts commit 2ff7848f5db4b817ae221962306d22d7c69c7be9.

--- db/schema.rb
+++ db/schema.rb
@@ -10,59 +10,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_30_134004) do
+ActiveRecord::Schema[7.2].define(version: 2026_06_30_134004) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
+  enable_extension "plpgsql"
 
   create_table "account_unlock_events", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.bigint "trn_request_id"
     t.datetime "updated_at", null: false
+    t.bigint "trn_request_id"
     t.index ["trn_request_id"], name: "index_account_unlock_events_on_trn_request_id"
   end
 
   create_table "audits1984_audits", force: :cascade do |t|
-    t.bigint "auditor_id", null: false
-    t.datetime "created_at", null: false
+    t.integer "status", default: 0, null: false
     t.text "notes"
     t.bigint "session_id", null: false
-    t.integer "status", default: 0, null: false
+    t.bigint "auditor_id", null: false
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["auditor_id"], name: "index_audits1984_audits_on_auditor_id"
     t.index ["session_id"], name: "index_audits1984_audits_on_session_id"
   end
 
   create_table "console1984_commands", force: :cascade do |t|
-    t.datetime "created_at", null: false
+    t.text "statements"
     t.bigint "sensitive_access_id"
     t.bigint "session_id", null: false
-    t.text "statements"
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["sensitive_access_id"], name: "index_console1984_commands_on_sensitive_access_id"
     t.index ["session_id", "created_at", "sensitive_access_id"], name: "on_session_and_sensitive_chronologically"
   end
 
   create_table "console1984_sensitive_accesses", force: :cascade do |t|
-    t.datetime "created_at", null: false
     t.text "justification"
     t.bigint "session_id", null: false
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["session_id"], name: "index_console1984_sensitive_accesses_on_session_id"
   end
 
   create_table "console1984_sessions", force: :cascade do |t|
-    t.datetime "created_at", null: false
     t.text "reason"
-    t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["created_at"], name: "index_console1984_sessions_on_created_at"
     t.index ["user_id", "created_at"], name: "index_console1984_sessions_on_user_id_and_created_at"
   end
 
   create_table "console1984_users", force: :cascade do |t|
+    t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username", null: false
     t.index ["username"], name: "index_console1984_users_on_username"
   end
 
@@ -70,41 +70,41 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_30_134004) do
   end
 
   create_table "features", force: :cascade do |t|
+    t.string "name", null: false
     t.boolean "active", default: false, null: false
     t.datetime "created_at", null: false
-    t.string "name", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_features_on_name", unique: true
   end
 
   create_table "staff", force: :cascade do |t|
-    t.datetime "confirmation_sent_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "created_at", null: false
-    t.datetime "current_sign_in_at"
-    t.string "current_sign_in_ip"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.integer "failed_attempts", default: 0, null: false
-    t.datetime "invitation_accepted_at", precision: nil
-    t.datetime "invitation_created_at", precision: nil
-    t.integer "invitation_limit"
-    t.datetime "invitation_sent_at", precision: nil
-    t.string "invitation_token"
-    t.integer "invitations_count", default: 0
-    t.bigint "invited_by_id"
-    t.string "invited_by_type"
-    t.datetime "last_sign_in_at"
-    t.string "last_sign_in_ip"
-    t.datetime "locked_at"
-    t.datetime "remember_created_at"
-    t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "invitation_token"
+    t.datetime "invitation_created_at", precision: nil
+    t.datetime "invitation_sent_at", precision: nil
+    t.datetime "invitation_accepted_at", precision: nil
+    t.integer "invitation_limit"
+    t.string "invited_by_type"
+    t.bigint "invited_by_id"
+    t.integer "invitations_count", default: 0
     t.index ["confirmation_token"], name: "index_staff_on_confirmation_token", unique: true
     t.index ["email"], name: "index_staff_on_email", unique: true
     t.index ["invitation_token"], name: "index_staff_on_invitation_token", unique: true
@@ -115,59 +115,59 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_30_134004) do
   end
 
   create_table "trn_requests", force: :cascade do |t|
-    t.boolean "awarded_qts"
-    t.datetime "checked_at", precision: nil
     t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "checked_at", precision: nil
     t.date "date_of_birth"
     t.string "email", limit: 510
-    t.string "first_name", limit: 510
-    t.boolean "from_get_an_identity", default: false
-    t.boolean "has_active_sanctions"
-    t.boolean "has_ni_number"
+    t.string "ni_number", limit: 510
     t.boolean "itt_provider_enrolled"
     t.string "itt_provider_name"
-    t.string "itt_provider_ukprn"
+    t.boolean "has_ni_number"
+    t.string "first_name", limit: 510
     t.string "last_name", limit: 510
-    t.boolean "name_changed"
-    t.string "ni_number", limit: 510
-    t.boolean "official_name_preferred"
-    t.string "preferred_first_name"
-    t.string "preferred_last_name"
     t.string "previous_first_name", limit: 510
     t.string "previous_last_name", limit: 510
-    t.datetime "submitted_at"
     t.string "trn"
-    t.string "trn_from_user"
-    t.datetime "updated_at", null: false
     t.integer "zendesk_ticket_id"
+    t.boolean "awarded_qts"
+    t.boolean "has_active_sanctions"
+    t.boolean "name_changed"
+    t.boolean "from_get_an_identity", default: false
+    t.string "trn_from_user"
+    t.string "itt_provider_ukprn"
+    t.string "preferred_first_name"
+    t.string "preferred_last_name"
+    t.boolean "official_name_preferred"
+    t.datetime "submitted_at"
   end
 
   create_table "trn_responses", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.json "raw_result", default: "{}"
     t.bigint "trn_request_id", null: false
+    t.json "raw_result", default: "{}"
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["trn_request_id"], name: "index_trn_responses_on_trn_request_id"
   end
 
   create_table "validation_errors", force: :cascade do |t|
-    t.datetime "created_at", null: false
     t.string "form_object", null: false
     t.jsonb "messages", default: "{}", null: false
     t.bigint "trn_request_id", null: false
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["messages"], name: "index_validation_errors_on_messages", using: :gin
     t.index ["trn_request_id"], name: "index_validation_errors_on_trn_request_id"
   end
 
   create_table "zendesk_delete_requests", force: :cascade do |t|
-    t.datetime "closed_at", null: false
-    t.datetime "created_at", null: false
-    t.string "enquiry_type"
-    t.string "group_name"
-    t.string "no_action_required"
     t.datetime "received_at", null: false
+    t.datetime "closed_at", null: false
     t.integer "ticket_id", null: false
+    t.string "enquiry_type"
+    t.string "no_action_required"
+    t.string "group_name"
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
